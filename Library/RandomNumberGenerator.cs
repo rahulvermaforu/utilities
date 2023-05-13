8b289e8f1317db186d6162f8a1ae40b9db685331
Rahul <rahulvermaforu@gmail.com>
2023-05-13 21:55:19 +1000

    Adding some code.

---
+++ Library/RandomNumberGenerator.cs
@@ -0,0 +1,11 @@
+﻿namespace Library
+{
+    public static class RandomNumberGenerator
+    {
+        static Random random = new Random();
+        public static int Generate()
+        {
+            return random.Next();
+        }
+    }
+}
